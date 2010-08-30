740336ae990e2011d189701cab3e0aa6ea544aa9
DeQiang.Hao <DeQiang.Hao@c72cbc0a-5ad8-5466-a711-628fb45aa622>
2010-08-30 01:58:01 +0000

    更新標題名稱

--- trunk/application/views/layout/base.php
+++ trunk/application/views/layout/base.php
@@ -2,7 +2,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>deep-ci</title>
+<title>CdeIgniter</title>
 
 <script type="text/javascript" src="<?=js_url()?>lib/jquery-1.4.2.min.js"></script>
 <script type="text/javascript" src="<?=js_url()?>common.js"></script>
